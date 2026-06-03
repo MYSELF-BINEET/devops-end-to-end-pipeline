afb1490196c139eee00ec846945cfb4f0b08c69b
Bineet Pradhan <bineetpradhan03@gmail.com>
2026-06-03 22:42:50 +0530

    Update jenkinsfile & update fixing pipeline @v22

--- startservers.sh
+++ startservers.sh
@@ -1,1 +1,6 @@
-ssh -i mykey ec2-user@$(awk '/\[prod\]/{getline; print $1}' inventory) 'sudo socat TCP4-LISTEN:8080,fork,su=nobody TCP4:192.168.49.2:31933' &
+# ssh -i mykey ec2-user@$(awk '/\[prod\]/{getline; print $1}' inventory) 'sudo socat TCP4-LISTEN:8080,fork,su=nobody TCP4:192.168.49.2:31933' &
+
+HOST=$(tail -n 1 inventory)
+
+ssh -i "$SSH_KEY" ec2-user@$HOST \
+"sudo socat TCP4-LISTEN:8080,fork,su=nobody TCP4:192.168.49.2:31933" &
